69342aa918dce4c3fc6ca01e894cc544dbdc3ef7
Roman <r.dyagel@flippingbook.com>
2021-01-29 22:33:24 +0300

    L4-Task2. Подключил log4net, реализовал интерфейсы (ILogger, ILoggerProvider) для его подключения к системе логирования

---
+++ Common/WebStore.Logger/Log4NetLoggerProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Repository;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Logger
+{
+    internal class Log4NetLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, ILogger> _loggers;
+        private readonly ILoggerRepository _loggerRepository;
+
+        public Log4NetLoggerProvider(string configFile)
+        {
+            _loggers = new ConcurrentDictionary<string, ILogger>();
+
+            _loggerRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+            log4net.Config.XmlConfigurator.Configure(_loggerRepository, new FileInfo(configFile));
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, 
+                catName => new Log4NetLogger(LogManager.GetLogger(_loggerRepository.Name, catName)));
+        }
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+            _loggerRepository.Shutdown();
+        }
+    }
+}
